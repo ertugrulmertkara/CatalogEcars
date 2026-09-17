7d67ed1f827be394ab0e07af08fc5e19f27e218d
radfe46511@gmail.com <ertugrul.kara@tbtk.gov.tr>
2026-09-17 15:23:26 +0300

    backend is completed.

---
+++ backend/server.js
@@ -0,0 +1,72 @@
+const express = require("express"); // hazır bir express çağırır , express : node.js'in işini kolaylaştıran bir araç
+const cors = require("cors"); //frontendin backend e istek atmasını sağlayan güvenlik ayarıdır normalde bir siteden başka siteye istek atmaya güvenlik kuralı uygulanır.  
+
+const app = express(); // expressi kur
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+let cars = []; // araçları ram'de tutuyoruz , veritabanı yok 
+let nextId = 1; // her yeni araça unique id
+
+app.get("/api/ping" , function(req,res){ // req: sipariş fişi , res: tepsi 
+    res.json({  message: "pong"});
+});
+
+app.listen(PORT, function(){
+    console.log("Sunucu Çalışıyor: http://localhost:" + PORT);
+});
+
+app.get("/api/cars", function(req,res){
+    res.json(cars);
+})
+
+app.post("/api/cars", function(req,res){
+const car = {
+    id: nextId++, // 1,1 arttır
+    brand: req.body.brand,
+    model: req.body.model,
+    year: req.body.year,
+    bodyType: req.body.bodyType,
+    drivetrain: req.body.drivetrain,
+    range: req.body.range,
+    battery: req.body.battery,
+    price: req.body.price,
+    status: req.body.status || "catalog", // default: catalog
+    imageUrl: req.body.imageUrl,
+    link: req.body.link,
+    note: req.body.note,
+    createdAt: Date.now(),
+
+  };
+  cars.push(car); // diziye ekle
+  res.status(201).json(car); // 201(başarıyla oluşturuldu), araç listesini gönder.
+}
+
+);
+
+app.delete("/api/cars:id", function(req,res){
+    const id = Number(req.params.id); // id string olarak gelir onu int e çevirir
+    const index = cars.findIndex(function(car){ //findIndex bulamazsa -1 dönderir.
+    return car.id === id;
+    });
+    if (index === -1){
+        return res.status(404).json({ error: "araç bulunamadı."})
+    }
+
+    cars.splice(index , 1);
+    res.json({message : "Araç Silinmiştir."})
+});
+
+app.patch("/api/cars/:id/status", function(req,res){
+    const id = Number(req.params.id);
+    const car = cars.find(function(c){
+        return c.id === id;
+    });
+
+    if(!car){
+        return res.status(404).json({error: "Araç Bulunamadı."});
+     }
+     car.status = req.body.status;
+     res.json(car);
+})
